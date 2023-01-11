3e0909e88f483aeff6090ac64b78019a98e58c0d
minjae <kmj0824@postech.ac.kr>
2023-01-11 16:55:02 +0900

    ✨ Chip class implementing
    A chip has two circuit objects, which means each one tier(die).

---
+++ src/dataStructures/D2DChip.cpp
@@ -0,0 +1,227 @@
+///////////////////////////////////////////////////////////////////////////////
+// Creator: Minjae Kim of CSDL, POSTECH
+// Email:   kmj0824@postech.ac.kr
+//
+// BSD 3-Clause License
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////
+
+#include "D2DChip.h"
+
+namespace VLSI_backend {
+void D2DChip::parse_iccad2022(const string &input_file_name) {
+  // open input file
+  ifstream input_file(input_file_name);
+  if (input_file.fail()) {
+    cerr << "Cannot open the input file: " << input_file_name << endl;
+    exit(1);
+  }
+
+  // parsing start.
+  string info, name1, name2;
+  int n1;
+  float f1, f2, f3, f4;
+
+  // check the number of Technologies
+  // Syntax of input file: NumTechnologies <technologyCount>
+  input_file >> info >> n1;
+  assert(info == "NumTechnologies");
+  this->num_technologies_ = n1;
+  circuits_.resize(this->num_technologies_);
+
+  dbTech *db_tech;
+  dbTechLayer *db_tech_layer;
+  dbLib *lib;
+  dbChip *chip;
+  dbBlock *block;
+
+  // read Technologies
+  /////////////////////////////////// lef parsing for each tier ///////////////////////////////////////
+  for (int i = 0; i < this->num_technologies_; ++i) {
+    // Syntax: Tech <techName> <libCellCount>
+    input_file >> info >> name1 >> n1;
+    assert(info == "Tech");
+
+    Circuit *circuit = &circuits_.at(i);
+    dbDatabase *db_database = circuit->getDbDatabase();
+
+    // create tech
+    // (refer to `dbDatabase* createSimpleDB()` submodule/OpenDB/tests/cpp/helper.cpp)
+    db_tech = dbTech::create(circuit->getDbDatabase());
+    db_tech_layer = dbTechLayer::create(db_tech, "metal1", dbTechLayerType::MASTERSLICE);
+    lib = dbLib::create(db_database, info.c_str(), ',');
+    chip = dbChip::create(db_database);
+    block = dbBlock::create(chip, "block");
+
+    // create libCells
+    circuit->setLibCellNum(n1);
+    for (int j = 0; j < circuit->getLibCellNum(); ++j) {
+      // Syntax: LibCell <libCellName> <libCellSizeX> <libCellSizeY> <pinCount>
+      input_file >> info >> name1 >> f1 >> f2 >> n1;
+      assert(info == "LibCell");
+      // create one libCell
+      // (refer to `dbDatabase* createSimpleDB()` submodule/OpenDB/tests/cpp/helper.cpp)
+      dbMaster *master = dbMaster::create(lib, name1.c_str());
+      master->setWidth(static_cast<int>(f1));
+      master->setHeight(static_cast<int>(f2));
+      master->setType(dbMasterType::CORE);
+      // create pins in the above libCell
+      // (refer to addGeoms in `OpenDB/src/lefin/lefin.cpp`)
+      for (int k = 0; k < n1; ++k) {
+        // Syntax: Pin <pinName> <pinLocationX> <pinLocationY>
+        // pin object generate
+        input_file >> info >> name1 >> f1 >> f2;
+        assert(info == "Pin");
+        dbMTerm *db_m_term = dbMTerm::create(master, name1.c_str(), dbIoType::INOUT, dbSigType::SIGNAL);
+        // pin geometry generate
+        dbMPin *db_m_pin = dbMPin::create(db_m_term);
+        int x1 = static_cast<int>(f1);
+        int x2 = x1 + 1;
+        int y1 = static_cast<int>(f2);
+        int y2 = y1 + 1;
+        dbBox::create(db_m_pin, db_tech_layer, x1, y2, x2, y2);
+        // trim the order of pins in the master terminal
+        dbSet<dbMPin> pins = db_m_term->getMPins();
+        if (pins.reversible() && pins.orderReversed())
+          pins.reverse();
+      }
+    }
+  }
+
+  // Syntax: DieSize <lowerLeftX> <lowerLeftY> <upperRightX> <upperRightY>
+  input_file >> info >> f1 >> f2 >> f3 >> f4;
+  assert(info == "DieSize");
+  // TODO
+//  circuit.topDie = Die("TopDie", f1, f2, f3, f4);
+//  circuit.bottomDie = Die("BottomDie", f1, f2, f3, f4);
+
+
+  // Syntax: TopDieMaxUtil <util>
+  input_file >> info >> f1;
+  assert(info == "TopDieMaxUtil");
+  // TODO
+//  circuit.topDie.maxUtil = f1;
+
+  // Syntax: BottomDieMaxUtil <util>
+  input_file >> info >> f1;
+  assert(info == "BottomDieMaxUtil");
+  // TODO
+//  circuit.bottomDie.maxUtil = f1;
+
+  // Syntax: TopDieRows <startX> <startY> <rowLength> <rowHeight> <repeatCount>
+  input_file >> info >> f1 >> f2 >> f3 >> f4 >> n1;
+  assert(info == "TopDieRows");
+  // TODO
+//  circuit.topDie.setRows(f1, f2, f3, f4, n1);
+
+  // Syntax: BottomDieRows <startX> <startY> <rowLength> <rowHeight>
+  // <repeatCount>
+  input_file >> info >> f1 >> f2 >> f3 >> f4 >> n1;
+  assert(info == "BottomDieRows");
+  // TODO
+//  circuit.bottomDie.setRows(f1, f2, f3, f4, n1);
+
+  // Syntax: TopDieTech <TechName>
+  input_file >> info >> name1;
+  assert(info == "TopDieTech");
+  // TODO
+//  circuit.topDie.tech = techs[name1];
+//  for (auto &libCell : circuit.topDie.tech.libCells) {
+//    libCell.dieID = DieID::TOP;
+//  }
+
+  // Syntax: BottomDieTech <TechName>
+  input_file >> info >> name1;
+  assert(info == "BottomDieTech");
+  // TODO
+//  circuit.bottomDie.tech = techs[name1];
+//  for (auto &libCell : circuit.bottomDie.tech.libCells) {
+//    libCell.dieID = DieID::BOTTOM;
+//  }
+
+  // Syntax: TerminalSize <sizeX> <sizeY>
+  input_file >> info >> f1 >> f2;
+  assert(info == "TerminalSize");
+  // TODO
+//  circuit.terminalSizeX = f1;
+//  circuit.terminalSizeY = f2;
+
+  // Syntax: TerminalSpacing <spacing>
+  input_file >> info >> f1;
+  assert(info == "TerminalSpacing");
+  // TODO
+//  circuit.terminalSpacing = f1;
+
+  // Syntax: NumInstances <instanceCount>
+  input_file >> info >> n1;
+  assert(info == "NumInstances");
+  // TODO
+//  circuit.numInstances = n1;
+//  circuit.netlist.setNumCell(n1);
+//  circuit.setBench(n1);
+
+
+  // read Instances in one circuit
+  // TODO
+//  for (int i = 0; i < circuit.numInstances; i++) {
+//    // Syntax: Inst <instName> <libCellName>
+//    input_file >> info >> name1 >> name2;
+//    assert(info == "Inst");
+//    auto &libCellTop = circuit.topDie.tech.getLibCell(name2);
+//    auto &libCellBottom = circuit.bottomDie.tech.getLibCell(name2);
+//    circuit.netlist.addCell(name1, libCellTop, libCellBottom);
+//  }
+
+  // Syntax: NumNets <netCount>
+  input_file >> info >> n1;
+  assert(info == "NumNets");
+  // TODO
+//  circuit.numNets = n1;
+//  circuit.netlist.setNumNet(n1);
+
+  // read Nets in one circuit
+  for (int i = 0; i < circuit.numNets; i++) {
+    // Syntax: Net <netName> <numPins>
+    input_file >> info >> name1 >> n1;
+    assert(info == "Net");
+    // TODO
+//    circuit.netlist.addNet(name1, n1);
+
+    // read pins in one Net
+    for (int j = 0; j < n1; j++) {
+      // Syntax: Pin <instName>/<libPinName>
+      input_file >> info >> name2;
+      assert(info == "Pin");
+      // TODO
+//      circuit.netlist.addPin(name1, name2);
+    }
+  }
+
+}
+}
+
