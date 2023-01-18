1293c64130dbca427c37b7aa167a8951797f4a58
minjae <kmj0824@postech.ac.kr>
2023-01-18 14:25:09 +0900

    Reset main function for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
+#include <igraph.h>
 #include "Circuit.h"
 
 using namespace std;
 
 int main() {
-  string lefName = "Nangate45.lef";
-  string defName = "simple01.def";
-  string test_path_name = "../test/benchmarks/";
-  string output_path_name = "../output/";
-
-  VLSI_backend::Circuit circuit;
-  circuit.parse(test_path_name + lefName, test_path_name + defName);
-  // circuit.dbTutorial();
-  circuit.place();
-  circuit.write(output_path_name +"output_"+ defName);
-
 }
